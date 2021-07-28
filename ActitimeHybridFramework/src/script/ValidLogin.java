9f4057b652d39ef858f4361c3c36949a19505bf0
admin <admin@192.168.0.14>
2021-07-28 17:37:07 +0530

    ghfxhrygxr

--- ActitimeHybridFramework/src/script/ValidLogin.java
+++ ActitimeHybridFramework/src/script/ValidLogin.java
@@ -9,7 +9,7 @@ import page.LoginPage;
 
 public class ValidLogin extends BaseTest{
 	
-	@Test(priority=1)
+	@Test(priority=1)//
 	public void testValidLogin() {
 		String un=XL.getData(XL_PATH, "ValidLogin", 1, 0);
 		String pw=XL.getData(XL_PATH, "ValidLogin", 1, 1);
